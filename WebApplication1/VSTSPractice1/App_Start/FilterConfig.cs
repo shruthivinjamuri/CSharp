eeb19eb90110baa3e11c9ee949bb9d49db6caa85
shruthivinjamuri <shruthi@uwm.edu>
2017-09-10 12:33:33 -0700

    Initial Commit

---
+++ WebApplication1/VSTSPractice1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VSTSPractice1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
